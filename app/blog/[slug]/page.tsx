d52e92571ffab4a087dfc25e331eb1c5e187c8d7
trngochan <85632212+trngochan@users.noreply.github.com>
2025-06-14 21:58:55 +0700

    h

--- app/blog/[slug]/page.tsx
+++ app/blog/[slug]/page.tsx
@@ -1,14 +1,13 @@
-type PageProps = {
+type Params = {
   params: {
     slug: string;
   };
 };
 
-export async function generateMetadata({ params }: PageProps) {
+export async function generateMetadata({ params }: Params) {
   return { title: `Post: ${params.slug}` };
 }
 
-export default function Page({ params }: PageProps) {
+export default function Page({ params }: Params) {
   return <h1>Slug: {params.slug}</h1>;
 }
-
